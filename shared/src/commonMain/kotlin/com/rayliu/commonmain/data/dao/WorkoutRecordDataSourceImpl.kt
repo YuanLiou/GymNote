e8d429a19ee7cfdcfbafb0613fc97d4ce333f9de
Ray Yuan Liu <louis383@gmail.com>
2023-06-23 12:53:02 +0800

    Add Dao interface to Workout Records

---
+++ shared/src/commonMain/kotlin/com/rayliu/commonmain/data/dao/WorkoutRecordDataSourceImpl.kt
@@ -0,0 +1,73 @@
+package com.rayliu.commonmain.data.dao
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import com.rayliu.commonmain.data.database.AppDatabase
+import com.rayliu.commonmain.data.database.RecordDetails
+import com.rayliu.commonmain.data.database.WorkoutRecord
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import org.koin.core.annotation.Factory
+import org.koin.core.annotation.Named
+
+@Factory
+class WorkoutRecordDataSourceImpl(
+    @Named("IO") private val ioDispatcher: CoroutineDispatcher,
+    @Named("Default") private val defaultDispatcher: CoroutineDispatcher,
+    appDatabase: AppDatabase
+) : WorkoutRecordDataSource {
+
+    private val recordQueries = appDatabase.workoutRecordQueries
+    private val detailsQueries = appDatabase.recordDetailsQueries
+
+    override fun getWorkoutRecords(id: Long): Flow<List<WorkoutRecord>> {
+        return recordQueries.getWorkoutRecords(id).asFlow().mapToList(defaultDispatcher)
+    }
+
+    override fun getWorkoutRecordDetails(workoutRecordId: Long): Flow<List<RecordDetails>> {
+        return detailsQueries.getRecordDetails(workoutRecordId).asFlow().mapToList(defaultDispatcher)
+    }
+
+    override suspend fun insertRecordDetails(
+        workoutRecordId: Long,
+        createAt: String,
+        weight: Double?,
+        reps: Long?,
+        time: String?,
+        distance: Double?
+    ) = withContext(ioDispatcher) {
+        detailsQueries.insertNewRecord(
+            id = null,
+            workoutRecordId = workoutRecordId,
+            createAt = createAt,
+            lastModified = createAt,
+            weight = weight,
+            reps = reps,
+            time = time,
+            distance = distance
+        )
+    }
+
+    override suspend fun updateRecordDetails(
+        id: Long,
+        lastModified: String,
+        weight: Double?,
+        reps: Long?,
+        time: String?,
+        distance: Double?
+    ) = withContext(ioDispatcher) {
+        detailsQueries.updateRecord(
+            id = id,
+            lastModified = lastModified,
+            weight = weight,
+            reps = reps,
+            time = time,
+            distance = distance
+        )
+    }
+
+    override suspend fun deleteRecordDetails(id: Long) = withContext(ioDispatcher) {
+        detailsQueries.deleteRecord(id)
+    }
+}
