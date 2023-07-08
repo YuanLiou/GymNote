d82561fc235befedcb2c9dce7ee74cf92bba494e
Ray Yuan Liu <louis383@gmail.com>
2023-07-08 14:14:02 +0800

    Add more workout type

--- shared/src/commonMain/kotlin/com/rayliu/commonmain/domain/model/SportRecordType.kt
+++ shared/src/commonMain/kotlin/com/rayliu/commonmain/domain/model/SportRecordType.kt
@@ -4,6 +4,10 @@ enum class SportRecordType(val id: Int) {
     WEIGHT_REPS(0),
     WEIGHT_TIME(1),
     DISTANCE_TIME(2),
+    WEIGHT(3),
+    REPS(4),
+    TIME(5),
+    DISTANCE(6),
     UNKNOWN(-1);
 
     companion object {
