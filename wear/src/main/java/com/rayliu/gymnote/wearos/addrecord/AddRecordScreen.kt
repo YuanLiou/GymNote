91a0eaed3bf5f777816d9c79d73204f12dbabb78
Ray Yuan Liu <louis383@gmail.com>
2023-07-08 15:04:46 +0800

    Add Pager to the add record page

--- wear/src/main/java/com/rayliu/gymnote/wearos/addrecord/AddRecordScreen.kt
+++ wear/src/main/java/com/rayliu/gymnote/wearos/addrecord/AddRecordScreen.kt
@@ -1,27 +1,70 @@
 package com.rayliu.gymnote.wearos.addrecord
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.Text
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.HorizontalPageIndicator
+import com.google.android.horologist.compose.pager.PageScreenIndicatorState
+import com.rayliu.gymnote.wearos.addrecord.recordinput.AddDistanceRecordScreen
+import com.rayliu.gymnote.wearos.addrecord.recordinput.AddRepsRecordScreen
+import com.rayliu.gymnote.wearos.addrecord.recordinput.AddTimeRecordScreen
+import com.rayliu.gymnote.wearos.addrecord.recordinput.AddWeightRecordScreen
 import com.rayliu.gymnote.wearos.theme.GymNoteTheme
 import com.rayliu.gymnote.wearos.theme.PreviewConstants
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AddRecordScreen(
     workoutId: String,
     modifier: Modifier = Modifier
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center,
+    Box(
         modifier = modifier.fillMaxSize()
     ) {
-        Text("Workout ID: $workoutId")
+        val pageCounts = 4
+        val state = rememberPagerState(
+            initialPage = 0,
+            initialPageOffsetFraction = 0f
+        ) {
+            pageCounts
+        }
+
+        HorizontalPager(
+            state = state,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            when (page) {
+                0 -> {
+                    AddWeightRecordScreen()
+                }
+                1 -> {
+                    AddRepsRecordScreen()
+                }
+                2 -> {
+                    AddTimeRecordScreen()
+                }
+                3 -> {
+                    AddDistanceRecordScreen()
+                }
+            }
+        }
+
+        val pagerScreenState = remember { PageScreenIndicatorState(state) }
+        HorizontalPageIndicator(
+            pageIndicatorState = pagerScreenState,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 10.dp)
+        )
     }
 }
 
